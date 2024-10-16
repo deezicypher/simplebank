29b0ad4c495ecaefac7280c704f4873490b10188
Deezi <deezitheviper@gmail.com>
2024-10-16 22:08:13 +0100

    update on account sql query

--- db/query/account.sql
+++ db/query/account.sql
@@ -14,7 +14,12 @@ WHERE id = $1 LIMIT 1;
 SELECT * FROM accounts
 ORDER BY id;
 
--- name: UpdateAccount :exec
+-- name: UpdateAccount :one
 UPDATE accounts
   set balance = $2
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts
 WHERE id = $1;
